7d33f8141b6e6a683f49f40cc2f75423c26a4a34
vishnu1646 <vishnulance98@gmail.com>
2022-11-21 10:33:01 +0530

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static int add(int num1,int num2){
+        int sum = num1+num2;
+    return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the number /n 1.- Addition 2.Subtraction 3-Multiplication");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        switch (a){
+            case 1:
+                System.out.println("Enter the Number : ");
+                Scanner sc1 = new Scanner(System.in);
+                Scanner sc2 = new Scanner(System.in);
+                System.out.println("Sum is "+add(sc1.nextInt(),sc2.nextInt()));
+        }
+    }
+}
